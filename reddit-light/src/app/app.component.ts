169fe318b7c595c3406b3fd9fb14956909f6153e
Eleonore Euzenes <eleonore.euzenes@gmail.com>
2025-07-28 18:08:06 +0200

    add subreddits slection

--- reddit-light/src/app/app.component.ts
+++ reddit-light/src/app/app.component.ts
@@ -1,29 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { RedditService } from './reddit.service';
 import { NgFor, CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [NgFor, CommonModule],
+  imports: [NgFor, CommonModule, FormsModule],
 })
+
 export class AppComponent implements OnInit {
+  subreddits: string[] = ['formula1', 'straykids', 'YukiTsunoda', 'RedBullRacing', 'OscarPiastri', 'FormulaDank', 'DragRaceFrance'];
+  selectedSubreddit: string = 'formula1'; // default
+
   posts: any[] = [];
   selectedPost: any = null;
   comments: any[] = [];
-  loading: boolean = true;           // loading posts
-  loadingComments: boolean = false;  // loading comments
+  loading: boolean = true;
+  loadingComments: boolean = false;
+  showComments: boolean = false;
 
   constructor(private redditService: RedditService) {}
 
   ngOnInit(): void {
-    this.fetchPosts();
+    this.fetchPosts(); // fetch initial subreddit (formula1)
   }
 
   fetchPosts(): void {
     this.loading = true;
-    this.redditService.getTopPosts().subscribe(response => {
+    this.selectedPost = null; // clear previous post if any
+    this.redditService.getTopPosts(this.selectedSubreddit).subscribe(response => {
       this.posts = response.data.children;
       this.loading = false;
     });
@@ -42,9 +49,14 @@ export class AppComponent implements OnInit {
   onSelectPost(post: any): void {
     this.selectedPost = post;
     this.loadingComments = true;
-    this.redditService.getComments(post.data.id).subscribe(response => {
+    this.showComments = false;
+
+    this.redditService.getComments(this.selectedSubreddit, post.data.id).subscribe(response => {
       this.comments = response[1].data.children.slice(0, 5);
       this.loadingComments = false;
+      setTimeout(() => {
+        this.showComments = true;
+      }, 10);
     });
   }
 
